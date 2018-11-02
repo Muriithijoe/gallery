bb760b8a273422aa1187a99603db42417445c0cb
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-11-02 12:39:16 +0300

    made  views

--- pics/models.py
+++ pics/models.py
@@ -21,7 +21,7 @@ class Photo(models.Model):
     photo = models.ImageField(upload_to = 'articles/', blank=True)
     description = models.TextField()
     location = models.ForeignKey(Location)
-    category = models.ForeignKey(Category)
+    category = models.ManyToManyField(categories)
     post_date = models.DateTimeField(auto_now_add=True)
 
     @classmethod
@@ -39,3 +39,21 @@ class Photo(models.Model):
     def search_by_title(cls,search_term):
         pics=cls.objects.filter(title__icontains=search_term)
         return pics
+
+class Location(models.Model):
+    name = models.CharField(max_length =90)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+class categories(models.Model):
+    name = models.CharField(max_length =60)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
