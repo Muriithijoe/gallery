1d0661d9d6246e4ca963a50a5d710dbd8a03a2bc
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-11-04 16:53:36 +0300

    custom made model

--- pics/admin.py
+++ pics/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Editor,Location,categories,Photo
 
+class PhotoAdmin(admin.ModelAdmin):
+    filter_horizontal =('categories',)
+
 admin.site.register(Editor)
 admin.site.register(Location)
 admin.site.register(categories)
