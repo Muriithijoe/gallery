4ccc8d4c306df20af0b0ef4a1e33a668216d08e9
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-11-04 16:40:24 +0300

    added admin

--- pics/admin.py
+++ pics/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Editor,Location,categories,Photo
 
-# Register your models here.
+admin.site.register(Editor)
+admin.site.register(Location)
+admin.site.register(categories)
+admin.site.register(Photo)
