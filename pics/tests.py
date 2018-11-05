15134bdee1591a5af5d4982ef438532d6c5493fc
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-11-05 08:15:17 +0300

    added tests

--- pics/tests.py
+++ pics/tests.py
@@ -1,1 +1,12 @@
 from django.test import TestCase
+from .models import Editor,Location,categories,Photo
+
+class EditorTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.james,Editor))
