eae5ffa942c0b22b1802402b896292b0049860cd
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-11-05 12:55:40 +0300

    strike 1

--- pics/tests.py
+++ pics/tests.py
@@ -16,3 +16,25 @@ class EditorTestClass(TestCase):
     # Testing  instance
     def test_instance(self):
         self.assertTrue(isinstance(self.james,Editor))
+
+class PhotoTestClass(TestCase):
+
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+        self.james.save_editor()
+
+        # Creating a new tag and saving it
+        self.new_category = categories(name = 'testing')
+        self.new_category.save()
+
+        self.new_photo= Photo(title = 'Test Article',post = 'This is a random test Post',editor = self.james)
+        self.new_photo.save()
+
+        self.new_photo.tags.add(self.new_tag)
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        Location.objects.all().delete()
+        categories.objects.all().delete()
+        Photo.objects.all().delete()
